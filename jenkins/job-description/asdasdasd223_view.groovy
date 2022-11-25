912e3d76fdee04bacdd7b6e436203ac9dc99738d
kkanto <108447856+kkanto@users.noreply.github.com>
2022-11-25 13:26:52 +0100

    Add new ADP toolchain configuration template

---
+++ jenkins/job-description/asdasdasd223_view.groovy
@@ -0,0 +1,16 @@
+ 
+listView('asdasdasd223 Jobs') {
+    description('asdasdasd223 Jobs')
+    jobs {
+        regex('asdasdasd223_.+')
+    }
+    columns {
+        status()
+        weather()
+        name()
+        lastSuccess()
+        lastFailure()
+        lastDuration()
+        buildButton()
+    }
+}
